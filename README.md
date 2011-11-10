fc44388c03aa7f3c9dcbc4fe199d249a2763f961
Luke Wendling <luke@lukewendling.com>
2011-11-10 15:09:31 -0600

    less eager db calling. update README

--- README.md
+++ README.md
@@ -2,42 +2,38 @@
 
 A very simple polymorphic implementation of name-value pairs for any ActiveRecord model. The value is serialized to allow storage of ruby objects.
 
-# Install
+## Install
 
-<pre>
-sudo gem install app_attributes
-</pre>
+  sudo gem install app_attributes
 
 OR
 
-<pre>
-gem 'app_attributes'
-bundle install
-</pre>
+  gem 'app_attributes'
+  bundle install
 
-# Usage
+## Usage
 
 in your model:
 
-<pre>
-class Author &lt; ActiveRecord::Base
-  include AppAttributes
+  class Author &lt; ActiveRecord::Base
+    include AppAttributes
 
-  ext_attribute :hat_size
-  # provides @author.hat_size, @author.hat_size=, @author.hat_size?
-end
-</pre>
+    ext_attribute :hat_size, 10, :to_i
+    # args: attribute name, default value, casting method for returned value
+    # provides @author.hat_size, @author.hat_size=, @author.hat_size?
+    # returns value cast as integer
+  end
 
-# TODO
+## TODO
 
 * automate db migration. currently, you must manually copy/rename migration file in app_attributes/db/migrate
 * build out tests likely as a nested Rails app
 
-# Copyright and license
+## Copyright and license
 
 Copyright (c) 2011 Luke Wendling, released under the New BSD License
 
-Contributors:
+### Contributors:
 
 *   Jeff Pihl
 *   Scott Robertson
--- lib/app_attributes.rb
+++ lib/app_attributes.rb
@@ -10,18 +10,28 @@ module AppAttributes
 
   # Example:
   # include AppAttributes
-  # ext_attribute :something_extra, 'none'
-  # ext_attribute :something_else
+  # ext_attribute :some_numeric_value, 0, :to_i
+  # ext_attribute :some_string_value, 'none'
   #
   module ClassMethods
-    def ext_attribute(name, default_value=nil)
+    def ext_attribute(name, default_value=nil, casting_method=nil)
       define_method("#{name}") do
-        if self.new_record?
+        if self.new_record? # search in-memory
           app_attr = app_attributes.detect{|oa| oa.name == "#{name}"}
         else
-          app_attr = app_attributes.find(:first, :conditions => {:name => "#{name}"})
+          if app_attributes.empty? # try the db again
+            app_attr = app_attributes.find(:first, :conditions => {:name => "#{name}"})
+          else # search in-memory
+            app_attr = app_attributes.detect{|oa| oa.name == "#{name}"}
+          end
         end
-        (app_attr && app_attr.value) || default_value
+        ret_val = (app_attr && app_attr.value) || default_value
+        # cast value, if requested
+        if ret_val
+          casting_method ? ret_val.send(casting_method) : ret_val
+        else
+          ret_val #nil
+        end
       end
 
       define_method("#{name}?") do
@@ -29,6 +39,7 @@ module AppAttributes
       end
 
       define_method("#{name}=") do |raw_value|
+        # handle input of boolean false, converting to 'false'
         new_value = raw_value == false ? 'false' : (raw_value.blank? ? default_value : raw_value)
         if self.new_record?
           return if new_value.blank? # fail silently if no default value and new value is blank
@@ -39,6 +50,7 @@ module AppAttributes
           app_attr = app_attributes.find_or_create_by_name("#{name}")
 #            new_value = '-' if new_value.blank? # validation prevents blank value
           app_attr.update_attributes(:value => new_value)
+          app_attributes(true) # reload in-memory objects
         end
         return app_attr.value
       end
